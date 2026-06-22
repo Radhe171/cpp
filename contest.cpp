338ccfd96329be36c1c72c1a7f930070b11ebdaf
radheshyam borban <radheshyam.cs241258@global.org.in>
2026-06-23 00:15:13 +0530

    Add files via upload
    main

---
+++ contest.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std ;
+// int main (){
+//     int x,y,z;
+//     cin>>x>>y>>z;
+//     int largest =x;
+//     if( y>largest){
+//         largest=y;
+//     }
+//     if ( z>largest){
+//         largest=z;
+//     }
+//     cout<<" The largest number is."<<largest;
+//     return 0;
+
+// }
+
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     for(int i=1;i<n;i++){
+//         if(n==0||n==1){
+//             cout<<n<<" is not a prime number";
+//         }
+//         if ( n==2||n==3){
+//             cout<<n<<" is a prime number ";
+//         }
+//         if( n%2==0||n%3==0){
+//             cout<<n<<" is not a prime number ";
+//         }
+//         for( int i=5;i*i<=n;i+=6){
+//             if(n%i==0||n%(i+2)==0){
+//                 cout<<n<<" is not a prime number ";
+//                 return 0;
+//             }
+//         }
+//     }
+//     cout<<n<<" is a prime number";
+//     return 0;
+// }
+
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     bool isprime=true;
+//     if(n<=1){
+//         cout<<n<<" is not a prime number ";
+//         return 0;
+//     }
+//     for( int i=2;i<n;i++){
+//         if(n%i==0){
+            
+//             isprime=false;
+//             break;
+            
+//         }
+        
+//     }
+//     if(isprime){
+//         cout<<n<<" is a prime number ";
+// return 0;
+//     }else{
+//         cout<<n<<" is not a prime number ";
+//     }
+// }
+
+int main(){
+    
+    
+}
