7cdb5a459a9d33ec16429027213cbd6a57e38a43
radheshyam borban <radheshyam.cs241258@global.org.in>
2026-06-23 23:51:45 +0530

    Add files via upload

---
+++ 05.cpp
@@ -0,0 +1,107 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    
+// Q1...
+// cout<<"*"<<endl;
+// cout<<"**"<<endl;
+// cout<<"***"<<endl;
+// cout<<"****"<<endl;
+//Q2.....
+// int a=5;
+// float b=5.555;
+// char c='A';
+//  cout <<a<< "  - is an integer "<<endl;
+//  cout<<b<< "  - is a float "<<endl;
+//  cout<<c << "  - is a char "<<endl;
+//Q04...... 
+//  int a =5;
+//  int b=6;
+//    int c=a+b;
+//  cout<< "addition is :-"   << c<<endl;
+ //Q05.....
+
+// int a=8;
+// int b=7;
+// cout<< "sum is :- "<< a+b<<endl;
+// cout<<"sub is :- " << a-b<<endl;
+// cout<<" multi is :- "<< a*b<<endl;
+// cout<<" div is :- "<< a/b<<endl;
+// Q06...
+// float length =4.4;
+// float width=8;
+// float area=length*width;
+// cout<< area<<endl;
+// Q07....
+// string name ;
+// cout<< "enter your name "<< endl;
+// cin>>name;
+// cout<< "Hallo ! "<< name << "   !!welcome in C++" <<endl;
+//Q08..... 
+// int i=6;
+// int s=9;
+// if ( i>s){
+//     cout<<i<< " is larger"<<endl;
+
+// } else {
+//     if (i<s){
+//         cout<<s<<"  is larger"<<endl;}
+//      else {
+//         if(i=s){
+//             cout<<i<<"="<<s<<endl;        }
+//      }}
+// }
+// Q09.....
+// float radius ;
+// float pi=3.14;
+
+// cout << "enter the radius :-"<<endl;
+// cin>>radius;
+// float area =  pi* pow( radius,2);
+// float circumfrence = 2*pi*radius ;
+//  cout << "Area Of Circle is :-"<< area<< endl;
+//  cout <<"Circumfrence Of Circle Is:-"<< circumfrence <<endl;
+//  return 0;
+// Q10....
+// int a=5;
+// int b=5;
+// cout << a+b<<endl;
+// cout<< a-b<<endl;
+// cout<<a/b<<endl;
+// cout<<a*b<<endl;
+// cout<<a%b<<endl;
+// Q11.......
+// int i=0;
+// cout <<i++<<endl;
+// cout <<++i<<endl;
+// cout << --i<<endl;
+// cout<< i--<<endl;
+// int a=6;
+// int b=7;
+// if (a==b){
+//     cout<<"both are equal"<<endl;
+
+// } else {
+//     // if(a!=b){
+//     //     cout<< " both are not equal"<<endl;
+
+//     // }else {
+//         if (a>=b){
+//             cout<<"a is larger than b  "<< endl;
+
+//         }else {
+//             if(a<=b){
+//                 cout<< " a is smaller than b "<<endl;
+
+//             }
+           
+//         }
+//         }   
+//      }
+// // }
+ 
+
+
+
+    
