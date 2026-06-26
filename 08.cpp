52f1d3732ba0ce804d5d0f056b9b924376d93400
radheshyam borban <radheshyam.cs241258@global.org.in>
2026-06-26 13:37:17 +0530

    Add files via upload

---
+++ 08.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){ 
+    int n=5;
+    int  sum=0;
+    for( int  i=1;i<=n;i++){
+        sum=sum+i;
+        
+
+    } /// sum of n number 
+
+    cout<<"sum is ="<<sum<<endl; }
+
+//  int mul;
+//  for(int i=1;i<=10;i++ ){
+//      mul= 5*i;
+//     cout<<mul<<endl;
+//  }
+// tabel of 5
+ 
+// int sum=0;
+// for ( int i=1;i<=100 ;i++){
+//     if(i%2==0)    sum=sum+i;    
+// }
+// cout<<sum;
+// sum of even numbers 1-100    
+//  int fac=1;
+// int n;
+// cout << "enter the number which you need factorial"<<endl;
+// cin >> n;
+//  for ( int i=1 ;i<=n;i++){
+//     fac=fac*i;
+//  }
+//  cout<< "factorial of given number is -" << fac << endl;
+// factorial of numbers 
+// for ( int i=1 ; i<=10;i++)
+// {
+
+// cout<< pow(i,2)<< endl;
+// } 
+//  print the sqr of 1-10
+// int a=0,b=1 ,next;
+// for (int  i=1;i<=10;i++){
+//     cout<<a<<"";
+//     next=a+b;
+//     a=b;
+//         b=next;      
+
+// for ( int i=1;i<=100;i++){
+//     if(i%3==0 && i%5==0){
+//         cout<<" the number divisible by both 3 and 5 "<<i<<endl;
+//     }
+//     else {
+//         if(i%3==0){
+//             cout<<"only divisible by 3 "<<i<<endl;
+//         }
+//         else {
+//             if (i%5==0){
+//                 cout<<"only divisible by 5 "<<i<<endl;
+//             }
+//         }
+//     }
+
+// }  
+
+// int count =0;
+// int number=567;
+// int rev=0;
+
+// int temp= number;
+
+
+// while (temp >0)
+// { 
+//    int digit = temp  % 10;
+//     rev=rev*10+digit;
+//    temp /=10; 
+    
+// }
+// cout<<"A rev of number is="<< rev<<endl;
+
+// }
+
+ 
+
+
+
