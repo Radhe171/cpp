a4ae6d6c99d7c6aa844531f4fd7491367b44894e
radheshyam borban <radheshyam.cs241258@global.org.in>
2026-07-01 23:29:42 +0530

    Add files via upload

---
+++ 16.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    // int arr[3]={1,2,3};
+    // int index=arr[0];
+
+    // int target =2;
+    // for ( int i =1 ; i<3 ;i++){
+    //     if (target==arr[i] ){
+    //         index=i;
+           
+    //     }
+    //     break;
+         
+    //     }
+    //     cout << " element in a index ="<< index<<endl;
+    // }        
+
+
+//     int arr[5]={ 1,2,3,4,5};
+//     int sum=0;
+//     for ( int i:arr)
+//     {
+        // sum=sum+ i;    // for each loop ;
+//     } 
+//     cout << "sum is"<< sum<<endl;
+ 
+// int arr[5]={1,2,3,4,5};
+// int mul=arr[0];
+// for ( int i=1;i<5;i++){
+//     mul*=arr[i];
+// }
+// cout<<"mul is a ="<<mul<< endl;
+
+//  int arr[5]={ 1,2,3,4,1};
+//   int maxx=arr[0];
+//  for ( int i=1;i<5;i++){
+//     if(maxx<arr[i]){
+//         maxx=arr[i];
+//     }
+
+
+//  } cout << maxx;
+
+// int arr[5]={ 2,3,45,6,8};
+// int minn=arr[0];
+// for ( int i=1;i<5;i++){
+
+//     if (minn>arr[i] )
+//     { minn=arr[i];}
+// }
+// cout<<minn<<endl;
+
+}
+
+    
+    
+   
+    
+
+    
+    
