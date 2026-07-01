ad8d1607ca77759ca717e301b306cc88aaad482d
radheshyam borban <radheshyam.cs241258@global.org.in>
2026-07-01 23:29:15 +0530

    Add files via upload

---
+++ 15.cpp
@@ -0,0 +1,139 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n=5;
+
+// for ( int i=0 ;i<=3;i++){
+//     for( int j=0;j<=3;j++){
+//         cout<<"*";
+//     } 
+//     cout<<endl;
+// }
+
+// for ( int i=0;i<=3;i++){
+    
+//     for(int j=0;j<=i;j++){
+//         cout<<"*";
+//     }
+//     cout<< endl;
+
+// }
+
+
+
+// for ( int i=5;i>0;i--){
+//     for(int j=1;j<=i;j++){
+//         cout<<j;
+//     }
+//     cout<< endl;
+// }
+
+// for ( int i=5;i>0;i--){
+//     for(int j=1;j<=i;j++){
+//         cout<<"*";
+//     }
+//     cout<< endl;
+// }
+
+// for (int i=0;i<=3;i++){
+//     for ( int j=0;j<=3;j++){
+//         cout<<i;
+//     }
+//     cout<<endl;
+// }
+
+
+// for ( int i=1;i<=4;i++){
+//     for(int j=0;j<=i;j++){
+//         cout<<i;
+//     } 
+//     cout<<endl; 
+// }
+
+
+
+
+
+
+// int coun=1;
+
+// for ( int i=1;i<=4;i++){
+//     for(int j=1;j<=i;j++){
+//         cout<<coun++;
+//     } 
+//     cout<<endl; 
+// }
+
+
+/*
+
+for(int i=1; i<=n ; i++){
+     for ( int j=1;j<=n-i;j++){
+        cout<<" ";
+     }
+     for ( int k=1;k<=(2*i-1) ;k++ )
+     {
+       cout<<"*"; 
+
+     }
+    cout<<endl;
+    }
+    
+    for(int i=n; i>=1 ; i--){
+     for ( int j=1;j<=n-i;j++){
+        cout<<" ";
+     }
+     for ( int k=1;k<=(2*i-1) ;k++ )
+     {
+       cout<<"*"; 
+
+     }
+cout<<endl; 
+    }*/
+
+
+
+/*
+
+
+
+for(int i=1; i<=n ; i++){
+     for ( int j=1;j<=n-i;j++){
+        cout<<" ";
+     }
+     for ( int k=1;k<=(2*i-1) ;k++ )
+     {
+       cout<<"*"; 
+
+     }
+    cout<<endl;
+    }
+    
+    for(int i=n-1; i>=1 ; i--){
+     for ( int j=1;j<=n-i;j++){
+        cout<<" ";
+     }
+     for ( int k=1;k<=(2*i-1) ;k++ )
+     {
+       cout<<"*"; 
+
+     }
+cout<<endl; 
+    }
+*/
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+}
